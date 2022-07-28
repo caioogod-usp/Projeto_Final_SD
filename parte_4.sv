9c5ef94bf39d09d003a06bbd2fe0ef02638085e2
ikuyorih9 <106600632+ikuyorih9@users.noreply.github.com>
2022-07-27 21:25:02 -0400

    Update parte_4.sv

--- parte_4.sv
+++ parte_4.sv
@@ -1,4 +1,4 @@
-module fsm (output reg [2:0] ch, output en_0, reset, input en_3, Vint_z, clk, input iniciar);
+module fsm (output reg [2:0] ch, output en_0, reset, input en_3, Vint_z, clk, iniciar);
   //ch[0]: ch_vm; ch[1]: ch_ref; ch[2]: ch_zr;
   always @ (negedge clk or posedge iniciar)
     begin
@@ -6,6 +6,7 @@ module fsm (output reg [2:0] ch, output en_0, reset, input en_3, Vint_z, clk, in
         begin
           en_0 = 1'b1;
           reset = 1'b0;
+          
           ch[0] = 1'b1;
           ch[1] = 1'b0;
           ch[2] = 1'b0;
@@ -21,10 +22,11 @@ module fsm (output reg [2:0] ch, output en_0, reset, input en_3, Vint_z, clk, in
           if (Vint_z)
             begin
               en_0 = 1'b0;
+              reset = 1'b1;
+              
               ch[0] = 1'b0;
               ch[1] = 1'b0;
               ch[2] = 1'b1;
-              reset = 1'b1;
             end
         end
     end
