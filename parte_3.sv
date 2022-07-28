f9511846c92f96ff08ae2cde258f896c700f7b43
caioogod-usp <110139147+caioogod-usp@users.noreply.github.com>
2022-07-27 23:15:13 -0300

    Update parte_3.sv

--- parte_3.sv
+++ parte_3.sv
@@ -27,16 +27,20 @@ module registrador (output reg [3:0] Qo, input [3:0] Qi, input clk, load);
     end
 endmodule
 
-module bcd27seg (output a, b, c, d, e, f, g, input [3:0] BCD);
-assign {a, b, c, d, e, f, g} = ( BCD == 4'b0000 ) ? 7'b1111110 : //0
-                               ( BCD == 4'b0001 ) ? 7'b0110000 : //1
-                               ( BCD == 4'b0010 ) ? 7'b1101101 : //2
-                               ( BCD == 4'b0011 ) ? 7'b1111001 : //3
-                               ( BCD == 4'b0100 ) ? 7'b0110011 : //4
-                               ( BCD == 4'b0101 ) ? 7'b1011011 : //5
-                               ( BCD == 4'b0110 ) ? 7'b1011111 : //6
-                               ( BCD == 4'b0111 ) ? 7'b1110000 : //7
-                               ( BCD == 4'b1000 ) ? 7'b1111111 : //8
-                               ( BCD == 4'b1001 ) ? 7'b1111011 : //9
-                               7'b0000001 ;
+module bcd27seg (output reg [6:0] DISP, input [3:0] BCD);
+  always @* begin
+    case (BCD)
+      4'b0000: DISP = 7'b1111110; //0
+      4'b0001: DISP = 7'b0110000; //1
+      4'b0010: DISP = 7'b1101101; //2
+      4'b0011: DISP = 7'b1111001; //3
+      4'b0100: DISP = 7'b0110011; //4
+      4'b0101: DISP = 7'b1011011; //5
+      4'b0110: DISP = 7'b1011111; //6
+      4'b0111: DISP = 7'b1110000; //7
+      4'b1000: DISP = 7'b1111111; //8
+      4'b1001: DISP = 7'b1111011; //9
+      default: DISP = 7'b000000 ;
+    endcase
+  end
 endmodule
