3824aef247c032f4c9332e809a9b7bf8d05193ba
ikuyorih9 <106600632+ikuyorih9@users.noreply.github.com>
2022-08-06 10:53:49 -0400

    Update parte_4.sv

--- Parte4/parte_4.sv
+++ Parte4/parte_4.sv
@@ -1,31 +1,33 @@
+//MODULO DA MÁQUINA DE ESTADOS.
 module fsm (output reg [2:0] ch, output en_0, reset, input en_3, Vint_z, clk, iniciar);
-  always @ (negedge clk or posedge iniciar)
+  always @ (posedge clk or posedge iniciar) //toda vez que ocorrer uma borda positiva de clock ou iniciar.
     begin
-      if (iniciar)
+      if (iniciar) //se sinal de iniciar foi recebido.
         begin
-          en_0 = 1'b1;
-          reset = 1'b0;
+          en_0 = 1'b1; //habilita a contagem.
+          reset = 1'b0;//desabilita o reinicio.
           
-          ch[0] = 1'b1;
-          ch[1] = 1'b0;
-          ch[2] = 1'b0;
+          ch[0] = 1'b1; //ativa a chave ch_vm, amplificador integrador carrega.
+          ch[1] = 1'b0; //desativa a chave ch_ref.
+          ch[2] = 1'b0; //desativa a chave ch_zr.
         end
       else
         begin
-          if (en_3)
+          if (en_3) //se o contador chegou no último valor da contagem
             begin
-              ch[0] = 1'b0;
-              ch[1] = 1'b1;
-              ch[2] = 1'b0;
+              ch[0] = 1'b0; //desativa a chave ch_vm.
+              ch[1] = 1'b1; //ativa a chave ch_ref, permite que o amplificador integrador descarregue.
+              ch[2] = 1'b0; //desativa a chave ch_zr.
             end
-          if (Vint_z)
+          if (Vint_z) //quando o amplificador integrador descarregar.
+            //Quando Vint_Z chega em 1, os registradores salvam o valor da contagem.
             begin
-              en_0 = 1'b0;
-              reset = 1'b1;
+              en_0 = 1'b0; //desabilita a contagem.
+              reset = 1'b1;//reinicia o sistema.
               
-              ch[0] = 1'b0;
-              ch[1] = 1'b0;
-              ch[2] = 1'b1;
+              ch[0] = 1'b0; //desativa a chave ch_vm.
+              ch[1] = 1'b0; //desativa a chave ch_ref.
+              ch[2] = 1'b1; //ativa a chave ch_zr, a saída Vint_z vai a zero.
             end
         end
     end
